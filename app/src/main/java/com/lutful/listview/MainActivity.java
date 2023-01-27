b6b88a6a95bae7e94b685bd1f8a2c11275c66b99
lutful bari <lutfulbarimusa@gmail.com>
2023-01-27 06:16:34 +0600

    Get .Put

--- app/src/main/java/com/lutful/listview/MainActivity.java
+++ app/src/main/java/com/lutful/listview/MainActivity.java
@@ -13,11 +13,15 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
 
     ListView myListView;
+    ArrayList <HashMap<String, String>> arrayList = new ArrayList<>();
+    HashMap <String, String> hashMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,18 +29,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         myListView = findViewById(R.id.listView);
 
+
+        hashMap = new HashMap<>();
+        hashMap.put("name", "Jubayer Hossain");
+        hashMap.put("mobile", "999");
+        hashMap.put("Email", "lutfulbarimusa@gmail.com");
+        arrayList.add(hashMap);
+
+
+
+
+        hashMap = new HashMap<>();
+        hashMap.put("name", "Musa");
+        hashMap.put("Mobile", "111");
+        arrayList.add(hashMap);
+
+        hashMap = new HashMap<>();
+        hashMap.put("name", "Musk");
+        hashMap.put("Mobile", "222");
+        arrayList.add(hashMap);
+
+        hashMap = new HashMap<>();
+        hashMap.put("name", "Mohammadsk");
+        hashMap.put("Mobile", "3333");
+        arrayList.add(hashMap);
+
         MyAdapter myAdaper = new MyAdapter();
         myListView.setAdapter(myAdaper);
 
+
     }
-//+++++++++++++++++++++++++++
+
+    //+++++++++++++++++++++++++++
 
     private class MyAdapter extends BaseAdapter{
 
         @Override
         public int getCount() {
-
-            return 5;
+            return arrayList.size();
         }
 
         @Override
@@ -50,21 +80,32 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        public View getView(int  i, View view, ViewGroup viewGroup) {
+        public View getView(int position, View view, ViewGroup viewGroup) {
 
            LayoutInflater layoutInflater = (android.view.LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
             View myView = LayoutInflater.inflate(R.layout.item, viewGroup, false);
 
-            ImageView imageView;
-            imageView = myView.findViewById(R.id.imageView);
-            TextView textView = myView.findViewById(R.id.textView);
 
-                imageView.setOnClickListener(new View.OnClickListener() {
+            ImageView = myView.findViewById(R.id.imageView);
+            TextView tvName = myView.findViewById(R.id.tvName);
+            TextView tvMobile = myView.findViewById(R.id.tvMobile);
+
+
+            HashMap<String, String> hashMap = arrayList.get(position);
+            String name = hashMap.get("name");
+            String mobile = hashMap.get("name");
+
+            tvName.setText( "name");
+            tvMobile.setText( "mobile");
+
+
+            View imageView;
+            imageView.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
 
-                        Toast.makeText(getApplicationContext(), "hi", Toast.LENGTH_LONG) .show();
+                        Toast.makeText(getApplicationContext(), "hi\n"+name, Toast.LENGTH_LONG) .show();
 
                     }
                 });
